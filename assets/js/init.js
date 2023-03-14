798e853074c0f0be3c197403f5be6346abc39623
Vanitha K <kvanitha780@gmail.com>
2023-03-14 12:47:17 +0530

    first commit

---
+++ assets/js/init.js
@@ -0,0 +1,12 @@
+(function($){
+  $(function(){
+
+    $('.sidenav').sidenav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   var elems = document.querySelectorAll('.collapsible');
+//   var instances = M.Collapsible.init(elems, options);
+// });
